4295863adee090e24634b5c46f68a068e5b774cc
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-03 18:27:57 -0500

    Entity models created for admin-interface.

---
+++ src/app/admin-interface/components/dashboards/dashboards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboards',
+  templateUrl: './dashboards.component.html',
+  styleUrl: './dashboards.component.scss'
+})
+export class DashboardsComponent {
+
+}
