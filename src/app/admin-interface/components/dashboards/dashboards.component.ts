e90ccae44fd540934c0e33cd18c6dcc85ec25c9b
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-09-26 13:54:36 -0500

    String values have been refactored. Constants developed.

--- src/app/admin-interface/components/dashboards/dashboards.component.ts
+++ src/app/admin-interface/components/dashboards/dashboards.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertifyService, MessageType, Position } from '../../../services/features/admin/services/alertify.service';
 import { SignalRService } from '../../../services/core/services/signal-r.service';
+import { ReceivedFunctions } from '../../../constants/received-functions';
+import { HubUrls } from '../../../constants/hub-urls';
 
 @Component({
   selector: 'app-dashboards',
@@ -10,10 +12,10 @@ import { SignalRService } from '../../../services/core/services/signal-r.service
 export class DashboardsComponent implements OnInit {
 
   constructor(private alertfyService: AlertifyService, private signalRService: SignalRService){
-    signalRService.start("https://localhost:7241/products-hub")
+    signalRService.start(HubUrls.ProductsHub)
   }
   ngOnInit(): void {
-    this.signalRService.on("ReceivedProductAddedMessage", message =>{
+    this.signalRService.on(ReceivedFunctions.ReceivedProductAddedMessageFunction, message =>{
       this.alertfyService.message(message, {
       position: Position.BottomRight,
       messageType: MessageType.Info
---
+++ src/app/constants/hub-urls.ts
@@ -0,0 +1,3 @@
+export enum HubUrls {
+    ProductsHub =  "https://localhost:7241/products-hub"
+}
