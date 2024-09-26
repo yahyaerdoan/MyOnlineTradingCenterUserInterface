d7447e812397d61a6f82a45e286f1c7c13a3858f
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-09-26 13:44:44 -0500

    Signal r service has been used.

--- src/app/admin-interface/components/dashboards/dashboards.component.ts
+++ src/app/admin-interface/components/dashboards/dashboards.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType, Position } from '../../../services/features/admin/services/alertify.service';
+import { SignalRService } from '../../../services/core/services/signal-r.service';
 
 @Component({
   selector: 'app-dashboards',
   templateUrl: './dashboards.component.html',
   styleUrl: './dashboards.component.scss'
 })
-export class DashboardsComponent {
+export class DashboardsComponent implements OnInit {
 
+  constructor(private alertfyService: AlertifyService, private signalRService: SignalRService){
+    signalRService.start("https://localhost:7241/products-hub")
+  }
+  ngOnInit(): void {
+    this.signalRService.on("ReceivedProductAddedMessage", message =>{
+      this.alertfyService.message(message, {
+      position: Position.BottomRight,
+      messageType: MessageType.Info
+      })
+    })
+  }
 }
