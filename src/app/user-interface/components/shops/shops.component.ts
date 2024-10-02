a071283a6054cc4bb426b25a27aeeb29e992f015
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-10-02 15:30:49 -0500

    Refactor: Pagination has been refactored.

--- src/app/user-interface/components/shops/shops.component.ts
+++ src/app/user-interface/components/shops/shops.component.ts
@@ -18,37 +18,49 @@ export class ShopsComponent implements OnInit {
   products!: ListProduct[];
   totalProductCount!: number;
   currentPageNo: number = 1;
-  currentPageSize: number= 8;
+  currentPageSize: number= 12;
   totalPageCount!: number;
   pageNumberList: number[] =[];
 
-   getProducts() {
+  getProducts() {
     this.activatedRouteService.params.subscribe(async params => {
       this.currentPageNo = parseInt(params["pageNo"] ?? 1);
-      const data: {totalProductCount: number, products: ListProduct[]} = await this.productService.read(
-        this.currentPageNo -1, this.currentPageSize, () => {
-        //successCallback
-      }, () => {
-        //errorCallback
-      });
+  
+      const data: { totalProductCount: number, products: ListProduct[] } = await this.productService.read(
+        this.currentPageNo - 1, 
+        this.currentPageSize, 
+        () => { /* successCallback */ }, 
+        () => { /* errorCallback */ }
+      );
+  
       this.products = data.products;
       this.totalProductCount = data.totalProductCount;
       this.totalPageCount = Math.ceil(this.totalProductCount / this.currentPageSize);
       this.pageNumberList = [];
-      
-      if (this.currentPageNo -4 <= 0)
-        for (let index = 1; index <= 9; index++)
+  
+      // Static Page 1 is always shown, but exclude it from the dynamic range.
+      // Don't add Page 1 to dynamic list to avoid duplication.
+  
+      // Pagination logic starts from Page 2.
+      if (this.currentPageNo - 4 <= 0) {
+        // If current page is close to the beginning, show pages from 2 to 8 (if available)
+        for (let index = 2; index <= Math.min(9, this.totalPageCount - 1); index++) {
           this.pageNumberList.push(index);
-
-      else if(this.currentPageNo +4 >= this.totalPageCount)
-        for (let index = this.totalPageCount -8; index <= this.totalPageCount; index++)
+        }
+      } else if (this.currentPageNo + 4 >= this.totalPageCount) {
+        // If current page is near the end, show pages ending at the last page.
+        for (let index = Math.max(2, this.totalPageCount - 8); index < this.totalPageCount; index++) {
           this.pageNumberList.push(index);
-        
-      else
-      for (let index = this.currentPageNo -4; index <= this.currentPageNo +4; index++)
-        this.pageNumberList.push(index);
-     
-
-    });   
+        }
+      } else {
+        // In the middle, dynamically calculate pages around the current page, excluding Page 1 and the last page.
+        for (let index = this.currentPageNo - 4; index <= this.currentPageNo + 4; index++) {
+          if (index > 1 && index < this.totalPageCount) {
+            this.pageNumberList.push(index);
+          }
+        }
+      }
+    });
   }
+  
 }
