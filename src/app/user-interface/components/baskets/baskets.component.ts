5bcd0048c378a838e179c059bf495a70e024dd31
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-10-14 06:09:13 -0500

    feat: Implement listing of basket items
    - Developed functionality to retrieve and display items in the basket.
    - Enabled seamless listing of basket items for better user visibility.

--- src/app/user-interface/components/baskets/baskets.component.ts
+++ src/app/user-interface/components/baskets/baskets.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BasesComponent, SpinnerType } from '../../../bases/bases.component';
+import { BasketItemService } from '../../../services/core/models/basket-item.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ListBasketItem } from '../../../contracts/basketItems/list-basket-item';
 
 @Component({
   selector: 'app-baskets',
   templateUrl: './baskets.component.html',
   styleUrl: './baskets.component.scss'
 })
-export class BasketsComponent {
-  
+export class BasketsComponent extends BasesComponent implements OnInit {
+
+  constructor(private basketItemService: BasketItemService, spinnerService: NgxSpinnerService) { super(spinnerService) }
+
+  ngOnInit(): void {
+    this.getBasketItemList();
+  }
+
+  basketItems: ListBasketItem[] = []
+
+  async getBasketItemList(): Promise<void> {
+    this.showSpinner(SpinnerType.BallScaleMultiple);
+    this.basketItems = await this.basketItemService.getBasketItems();
+    this.hideSpinner(SpinnerType.BallScaleMultiple);
+  };
 
 }
